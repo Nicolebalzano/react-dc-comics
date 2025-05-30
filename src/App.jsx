06a71500bb43f9145b0e1aa413a231d31ced732d
Nicolebalzano <nicole.balzano03@gmail.com>
2025-05-30 16:45:38 +0200

    feat:header modified

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-    <h1>Ciao</h1>
+    <Header />
     </>
   )
 }
--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,4 +1,18 @@
 function Header(){
-    return
+    return <header className="header">
+        <img src="/dc-logo.png" alt="" className= "col-30"/>
+        <ul className= "col-70">
+            <li><a href="">Character</a></li>
+            <li className="active"><a href="">Comics</a></li>
+            <li><a href="">Movies</a></li>
+            <li><a href="">Tv</a></li>
+            <li><a href="">Games</a></li>
+            <li><a href="">Collectibles</a></li>
+            <li><a href="">Collectibles</a></li>
+            <li><a href="">Fans</a></li>
+            <li><a href="">News</a></li>
+            <li><a href="">Shop</a></li>
+        </ul>
+    </header>
 }
 export default Header
