9fb20602f2187b72bcdda86fddf29767ed4202e7
Nicolebalzano <nicole.balzano03@gmail.com>
2025-06-03 21:29:38 +0200

    feat:card addedd with map

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,7 +1,20 @@
+import comics from "./comics"
 function Main() {
     return <main className="main">
         <div className="container">
 <button className="main-btn"><a href="">CURRENT SERIES</a></button>
+{/* CARDS */}
+<div className="cards">
+    <div className="flex-card">
+    {comics.map ((curCard) => (
+        <div className="col-2">
+        <div className="thumb"><img src={curCard.thumb} alt={curCard.title} /></div>
+          <div className="card-title">{curCard.title}</div>
+        </div>
+    ))}
+    </div>
+</div>
+{/* /CARDS */}
         </div>
 
     </main>
