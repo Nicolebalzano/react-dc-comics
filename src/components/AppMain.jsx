2ccd6e83a1f2917e6d4e6bc83092cef463799a8f
Nicolebalzano <nicole.balzano03@gmail.com>
2025-06-03 21:45:20 +0200

    feat:button added to main

--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -2,19 +2,22 @@ import comics from "./comics"
 function Main() {
     return <main className="main">
         <div className="container">
-<button className="main-btn"><a href="">CURRENT SERIES</a></button>
-{/* CARDS */}
-<div className="cards">
-    <div className="flex-card">
-    {comics.map ((curCard) => (
-        <div className="col-2">
-        <div className="thumb"><img src={curCard.thumb} alt={curCard.title} /></div>
-          <div className="card-title">{curCard.title}</div>
-        </div>
-    ))}
-    </div>
-</div>
-{/* /CARDS */}
+            <button className="main-btn"><a href="">CURRENT SERIES</a></button>
+            {/* CARDS */}
+            <div className="cards">
+                <div className="flex-card">
+                    {comics.map((curCard) => (
+                        <div className="col-2">
+                            <div className="thumb"><img src={curCard.thumb} alt={curCard.title} /></div>
+                            <div className="card-title">{curCard.title}</div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            {/* /CARDS */}
+            <div className="flex-more-btn">
+                <button className="more-btn"><a href="">LOAD MORE</a></button>
+                </div>
         </div>
 
     </main>
