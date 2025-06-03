f78c6d76be05f69a80d5d1fdfd57ad99cc89fd3d
Nicolebalzano <nicole.balzano03@gmail.com>
2025-06-03 22:19:15 +0200

    feat:blue Stripe modified

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -4,16 +4,16 @@ function Header(){
             <div className="flex-col">
         <img src="/dc-logo.png" alt="" className= "col-30"/>
         <ul className= "col-70">
-            <li><a href="">Character</a></li>
+            <li><div><a href="">Character</a></div></li>
             <li className="active"><a href="">Comics</a></li>
-            <li><a href="">Movies</a></li>
-            <li><a href="">Tv</a></li>
-            <li><a href="">Games</a></li>
-            <li><a href="">Collectibles</a></li>
-            <li><a href="">Collectibles</a></li>
-            <li><a href="">Fans</a></li>
-            <li><a href="">News</a></li>
-            <li><a href="">Shop</a></li>
+            <li><div><a href="">Movies</a></div></li>
+            <li><div><a href="">Tv</a></div></li>
+            <li><div><a href="">Games</a></div></li>
+            <li> <div><a href="">Collectibles</a></div></li>
+            <li> <div><a href="">Collectibles</a></div></li>
+            <li> <div><a href="">Fans</a></div></li>
+            <li> <div><a href="">News</a></div></li>
+            <li> <div><a href="">Shop</a></div></li>
         </ul>
         </div>
         </div>
