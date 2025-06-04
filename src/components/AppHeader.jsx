1d3e60aa46d3200dff92f5fecee0d47db4d089c2
Nicolebalzano <nicole.balzano03@gmail.com>
2025-06-04 15:26:54 +0200

    feat:key added to nav list

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,30 +1,39 @@
 function Header(){
-    const menuVoices = [
+   const menuVoices = [
         {
+          id: 1,
             title : "Character"
         },
           {
+              id: 2,
             title : "Comics"
         },
           {
+              id: 3,
             title : "Movies"
         },
           {
+              id: 4,
             title : "TV"
         },
           {
+              id: 5,
             title : "Games"
         },
           {
+              id: 6,
             title : "Collectibles"
         },
         {
+            id: 7,
             title : "Fans"
         },
           {
+              id: 8,
             title : "News"
         },
           {
+              id: 9,
             title : "Shop"
         }
     ]
@@ -33,12 +42,17 @@ function Header(){
             <div className="flex-col">
         <img src="/dc-logo.png" alt="" className= "col-30"/>
         <ul className= "col-70">
-            {menuVoices.map((curVoice) =>
-            <li><div><a href="">{curVoice.title}</a></div></li> 
+            {menuVoices.map((curVoice, index) =>
+            <li key={`${curVoice.id}`}>
+              <div>
+                <a href="">{curVoice.title}</a>
+                </div>
+              </li> 
             )}
         </ul>
         </div>
         </div>
+        
     </header>
 }
 export default Header
